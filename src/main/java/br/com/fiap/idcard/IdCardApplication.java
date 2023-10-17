a4e28311d68157ce69cd86b4f400842351b7431b
Denis Benjamim <denis.benjamim@gmail.com>
2023-10-16 21:40:29 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fiap/idcard/IdCardApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.idcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdCardApplication.class, args);
+	}
+
+}
